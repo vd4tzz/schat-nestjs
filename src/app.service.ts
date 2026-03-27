8bd58f07a773508482ce2ed5e3f7ab1616620d6a
d4tzz <d4tzz@temp.com>
2026-03-27 07:55:56 +0700

    feat: setup prisma

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './shared/prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getHello() {
+    const users = await this.prisma.user.findMany();
+    return users;
   }
 }
